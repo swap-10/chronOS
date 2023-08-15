4ca825e6a9d3146a8dd21992fa002569e226a892
Swapnal Varma <swapnalv10@gmail.com>
2023-08-15 17:44:07 +0530

    Define basic types and system-wide params

---
+++ kernel/param.h
@@ -0,0 +1,9 @@
+#define NPROC 64  // max number of processes
+#define NCPU 8  // max number of CPUs
+#define NOFILE 16  // open files per process
+#define NFILE 100  // open files per system
+#define NINODE 50  // max number of active i-nodes
+#define NDEV 10  // maximum major device number
+#define NBUF (MAXOPBLOCKS*3)  // size of disk block cache
+#define FSSIZE 2000  // size of file system in blocks
+#define MAXPATH 128  // max file path name length
